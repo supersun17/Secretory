b8fccdb041cbcf47d3feb953f50c218412ffe4d9
Ming Sun <mingsun.nolan@gmail.com>
2019-06-05 18:56:03 -0400

    cocoapods upgrade

---
+++ com.MingSun.Secretory/Utilities/Credentials.swift
@@ -0,0 +1,18 @@
+//
+//  Credentials.swift
+//  com.MingSun.Secretory
+//
+//  Created by Ming Sun on 6/5/19.
+//
+
+import Foundation
+
+struct AMCCredentials {
+    let key = "2A33D456-AB6C-4ED2-9111-FA1411E7817F"
+    let keyHeader = "X-AMC-Vendor-Key"
+}
+
+struct TMDBCredentials {
+    let key = "9945fb60094629310a23b400af9ff4a8"
+    let keyQuery = "api_key"
+}
